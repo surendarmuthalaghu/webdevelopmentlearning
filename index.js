7389e6ab526d67f6404f57ad7b884001af239c4a
surendarmuthalaghu <129649142+surendarmuthalaghu@users.noreply.github.com>
2023-04-02 12:46:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+//Creating a package.json
+//In the project directory open termnial
+
+
+//Variable
+
+//1.var
+//2.let
+//3.const
+var Name = "surendar"
+
+
+Name = "Mithun"
+
+console.log(Name);
+
+let myName ="surya"
+
+myName="arya"
+console.log(myName);
+
+// Function 
